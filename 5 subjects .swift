155d21ee6808374e4a38f768831ea86cb4d4fbed
vaish-sahu <93263744+vaish-sahu@users.noreply.github.com>
2021-12-09 23:06:17 +0530

    marks in five subjects

---
+++ 5 subjects .swift
@@ -0,0 +1,20 @@
+var marks = 90
+
+switch marks {
+
+case 90:
+    print("pass with 69 in hindi")
+case 80:
+    print("pass with 79 in english")
+case 70:
+    print("pass with 50 in science")
+case 60:
+    print("pass with 60 in mathematics")
+case 50:
+    print("pass with 75 social Science")
+    
+default:
+    print("he/she didn't get such marks in any subject") 
+
+}
+
