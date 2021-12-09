f7a2a5052868ec0154f87130f0ce004038b6b020
vaish-sahu <93263744+vaish-sahu@users.noreply.github.com>
2021-12-09 23:19:20 +0530

    Update 5 subjects .swift

--- 5 subjects .swift
+++ 5 subjects .swift
@@ -1,18 +1,18 @@
 WAP to show the marks obtained by students in five subject and print subject wise? 
 
-var marks = 90
+var marks = 60
 
 switch marks {
 
-case 90:
+case 69:
     print("pass with 69 in hindi")
-case 80:
+case 79:
     print("pass with 79 in english")
-case 70:
+case 50:
     print("pass with 50 in science")
 case 60:
     print("pass with 60 in mathematics")
-case 50:
+case 75:
     print("pass with 75 social Science")
     
 default:
