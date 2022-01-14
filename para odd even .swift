2456884317e6dcd909ed8a2d1e281f84ab8ccd00
vaish-sahu <93263744+vaish-sahu@users.noreply.github.com>
2022-01-14 21:06:29 +0530

    para odd ..

---
+++ para odd even .swift
@@ -0,0 +1,11 @@
+
+  var numbers = [ 23,34, 56, 49,23 ,3]
+  for digits:  Int in numbers{
+ if digits % 2==0 {
+
+ print(" \(digits) is an even number.") 
+} else {
+    print(" \(digits) is an odd num.")
+
+}
+}
